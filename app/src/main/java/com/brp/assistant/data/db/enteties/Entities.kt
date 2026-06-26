2d2b305ba687c065733306377c1aaca4e0fddf16
Maks <100788318+MaksKbz@users.noreply.github.com>
2026-06-26 14:26:09 +0500

    cleanup: удалена папка enteties/ с опечаткой
    Все entity-классы давно перенесены в правильный пакет:
      com.brp.assistant.data.db.entities
    
    Stub-файл с комментарием "TODO: delete" удалён.
    Папка enteties/ исчезнет вместе с последним файлом в ней.

--- app/src/main/java/com/brp/assistant/data/db/enteties/Entities.kt
+++
@@ -1,8 +0,0 @@
-// This file is intentionally left as a redirect comment.
-// The correct package is com.brp.assistant.data.db.entities (no typo).
-// All classes (BrpModel, Accessory, KnowledgeCard, FaultCode, etc.)
-// are defined in: app/src/main/java/com/brp/assistant/data/db/entities/Entities.kt
-//
-// DO NOT add code here. This stub exists only to preserve git history.
-// TODO: delete this folder in a future cleanup branch.
-package com.brp.assistant.data.db.enteties
